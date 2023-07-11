8fdafcd2427d1960c5177252086111fb174d393e
Harley Shugart <harley.shugart@hotmail.com>
2023-07-11 09:23:13 -0400

    install and configure tailwind

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,7 @@ import { createApp } from 'vue';
 import App from '@/App.vue';
 import PrimeVue from 'primevue/config';
 import router from './router';
-
-import 'primevue/resources/themes/tailwind-light/theme.css';
+import './index.css'
 
 const app = createApp(App);
 
