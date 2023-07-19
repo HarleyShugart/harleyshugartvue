c672d11d7d0cec5c1e28f355b88a613dd8e01660
Harley Shugart <harley.shugart@hotmail.com>
2023-07-19 14:38:27 -0400

    Add and format board, allow moves

---
+++ src/components/TicTacToe/Board.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import Box from './Box.vue';
+</script>
+
+<template>
+    <div class="grid grid-cols-3 aspect-square">
+        <div class="border-b-4 border-r-4 flex">
+            <Box></Box>
+        </div>
+        <div class="border-b-4 border-r-4 flex">
+            <Box></Box>
+        </div>
+        <div class="border-b-4 flex">
+            <Box></Box>
+        </div>
+        
+        <div class="border-b-4 border-r-4 flex">
+            <Box></Box>
+        </div>
+        <div class="border-b-4 border-r-4 flex">
+            <Box></Box>
+        </div>
+        <div class="border-b-4 flex">
+            <Box></Box>
+        </div>
+        
+        <div class="border-r-4 flex">
+            <Box></Box>
+        </div>
+        <div class="border-r-4 flex">
+            <Box></Box>
+        </div>
+        <div class="flex">
+            <Box></Box>
+        </div>
+        
+    </div>
+</template>
---
+++ src/components/TicTacToe/Box.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { ref, Ref } from 'vue';
+
+const box: Ref<HTMLElement> = ref();
+
+const setContent = (move: string): void => {
+    box.value.innerHTML = move;
+};
+
+const insertO = (): void => {
+    setContent('O');
+};
+
+const insertX = (): void => {
+    setContent('X');
+};
+</script>
+
+<template>
+    <div 
+        ref="box"
+        class="grow self-center"
+        @mousedown.right="insertO"
+        @mousedown.left="insertX"
+    >
+        &nbsp;
+    </div>
+</template>
